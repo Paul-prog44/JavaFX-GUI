e473d287aa3eac340710fe34ca2e26a9dd06d501
Paul-prog44 <saintouenfr@hotmail.com>
2024-05-03 11:40:12 +0200

    Add audioClip app

---
+++ src/MultimediaPrograms/AudioApplication.java
@@ -0,0 +1,23 @@
+package MultimediaPrograms;
+
+import javafx.application.Application;
+import javafx.scene.media.AudioClip;
+import javafx.stage.Stage;
+
+
+public class AudioApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        AudioClip sound = new AudioClip("file:C:/Users/Gérald/Downloads/front-desk-bells-daniel_simon.wav");
+        sound.play();
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
