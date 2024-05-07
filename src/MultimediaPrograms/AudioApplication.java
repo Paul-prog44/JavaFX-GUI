96b1f49dfee3aa368e45cd9a7b4183196b128d83
Paul-prog44 <saintouenfr@hotmail.com>
2024-05-07 10:31:56 +0200

    Layout and Ship to Asteroid project

--- src/MultimediaPrograms/AudioApplication.java
+++ src/MultimediaPrograms/AudioApplication.java
@@ -17,7 +17,7 @@ public class AudioApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch(AudioApplication.class);
     }
 
 }
---
+++ src/asteroidGame/AsteroidsApplication.java
@@ -0,0 +1,67 @@
+package asteroidGame;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application {
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		Pane pane = new Pane();
+		pane.setPrefSize(600, 400);
+		
+		Polygon ship = new Polygon(-5, -5, 10, 0, -5, 5);
+		ship.setTranslateX(300);
+		ship.setTranslateY(200);
+		ship.setRotate(30);
+		
+		pane.getChildren().add(ship);
+		
+		Scene scene = new Scene(pane);
+		
+		Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
+		
+		scene.setOnKeyPressed(event -> {
+			pressedKeys.put(event.getCode(), Boolean.TRUE);
+		});
+		
+		scene.setOnKeyReleased(event -> {
+			pressedKeys.put(event.getCode(), Boolean.FALSE);
+		});
+		
+		Point2D movement = new Point2D(1, 0);
+		
+		new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				if (pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+					ship.setRotate(ship.getRotate() -5);
+				}
+				
+				if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+					ship.setRotate(ship.getRotate() +5);
+				}
+				
+				ship.setTranslateX(ship.getTranslateX() + movement.getX());
+			}
+		}.start();
+		
+		stage.setTitle("Asteroids !");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
